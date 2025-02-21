1179558ed0e6c8d93e66e281d6343bbeeab8463f
stvit09 <stvit09@gmail.com>
2025-02-21 15:47:03 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const imageUrls = [
+  "https://minniecamper02212025.s3.amazonaws.com/images/20240702_181644.jpg",
+	  "https://minniecamper02212025.s3.amazonaws.com/images/20240702_181648.jpg",
+	  "https://minniecamper02212025.s3.amazonaws.com/images/20240709_155333.jpg",
+];
+	
+let currentIndex = 0;
+
+function showImage(index) {
+  const imageElement = document.getElementById("carousel-image");
+  imageElement.src = imageUrls[index];
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % imageUrls.length;
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+  showImage(currentIndex);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showImage(currentIndex);
+});
